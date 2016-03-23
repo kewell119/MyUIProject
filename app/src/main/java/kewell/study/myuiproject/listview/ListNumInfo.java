308af88373355bea1aa5662522462dbe0cbf24e2
kewell119 <kewell119@hotmail.com>
2016-03-23 17:12:21 +0800

    修改ListView示例二

--- app/src/main/java/kewell/study/myuiproject/listview/ListNumInfo.java
+++ app/src/main/java/kewell/study/myuiproject/listview/ListNumInfo.java
@@ -4,8 +4,6 @@ package kewell.study.myuiproject.listview;
  * Created by Administrator on 2016-03-22.
  */
 public class ListNumInfo {
-
-    private String tetetee;
     private String mNumName;
     private boolean mIsChecked = false;
 
